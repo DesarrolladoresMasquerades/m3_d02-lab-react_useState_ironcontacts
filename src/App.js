8b5007c5eb702ad2646b73410ea771737a302421
javisastre <76698419+javisastre@users.noreply.github.com>
2021-12-01 09:28:55 +0100

    Lab's solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import contacts from "./contacts.json";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+const remainingContacts = [...contacts];
+const initAgenda = remainingContacts.splice(0, 5);
+
+const App = () => {
+  const [agenda, setAgenda] = useState(initAgenda);
+
+  const addContact = () => {
+    const rnd = Math.floor(Math.random() * remainingContacts.length);
+    const randomContact = remainingContacts.splice(rnd, 1);
+    setAgenda(agenda.concat(randomContact));
+  };
+
+  const sortPopularity = () => {
+    const sortedAgenda = agenda
+      .sort((a, b) => b.popularity - a.popularity)
+      .slice();
+
+    setAgenda(sortedAgenda);
+  };
+
+  const sortName = () => {
+    const sortedAgenda = agenda.sort((a, b) =>
+      a.name.toUpperCase().localeCompare(b.name.toUpperCase())
+    );
+
+    setAgenda([...sortedAgenda]);
+  };
+
+  const deleteContact = (contactId) => {
+    const filteredAgenda = agenda.filter((contact) => contact.id !== contactId);
+    setAgenda(filteredAgenda);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>IronContacts</h1>
+      <div className='buttons'>
+        <button onClick={addContact}>Add Random Contact</button>
+        <button onClick={sortPopularity}>Sort by popularity</button>
+        <button onClick={sortName}>Sort by name</button>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {agenda.map((contact, i) => {
+            return (
+              <tr key={i}>
+                <td>
+                  <img
+                    src={contact.pictureUrl}
+                    alt=''
+                    className='table-image'
+                  />
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity.toFixed(2)}</td>
+                <td>{contact.wonOscar && "🏆"}</td>
+                <td>{contact.wonEmmy && "🏆"}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      deleteContact(contact.id);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
-}
-
+};
 export default App;
